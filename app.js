315863e3b3055404d63b131e343632f7700f0a9e
Alena <chumakalena.86@gmail.com>
2021-01-24 22:33:06 +0100

    added scroll

--- app.js
+++ app.js
@@ -17,11 +17,15 @@ let moreDetailsBtns = document.querySelectorAll(".more-details");
 let closeBtn = document.querySelector(".btn-close");
 
 moreDetailsBtns.forEach((btn) => {
-	btn.addEventListener("click", function () {
-		modal.classList.add("show");
-		modal.classList.remove("hide");
-	})	
+	btn.addEventListener("click", openModal) 
+			
 })
+
+function openModal() {
+	modal.classList.add("show");
+	modal.classList.remove("hide");
+}	
+
 
 function closeModal() {
 	modal.classList.add("hide");
@@ -34,5 +38,38 @@ modal.addEventListener("click", function (e) {
 	if (e.target == modal) {
 		closeModal()
 	}
+	
+})
+
+function showModalByScroll() {
+	if (window.pageYOffset > document.body.scrollHeight/2) {
+		openModal();
+		window.removeEventListener("scroll",showModalByScroll)
+	}
+	
+}
+window.addEventListener("scroll",showModalByScroll);
+
+// console.log(window.pageYOffset)
+// console.log(document.body.scrollHeight)
+
+
+
+// change like button state
+
+let likeButtons = document.querySelectorAll('.img-heart');
+
+likeButtons.forEach((btn) => {
+	btn.addEventListener("click", function (e) {
+		this.classList.toggle("liked")
+	})
+
+
+	// btn.addEventListener("click", function () {
+	// 	if (this.classList.contains("liked")) {
+	// 		this.classList.remove("liked")
+	// 	} else {
+	// 		this.classList.add("liked")
+	// 	}
 	
 })
